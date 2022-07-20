cfa9356e9dd970cdfc0d961917dd3a42ae159ce2
Johnny <johnny@edgeservpos.com>
2022-07-20 21:26:04 +0200

    Assigned imageUrls and random places to drivers

--- backend/server.js
+++ backend/server.js
@@ -6,11 +6,26 @@ const fs = require('fs');
 const app = express()
 const PORT = process.env.PORT || 5000
 
-const dir = path.join(__dirname, 'public');
-app.use(express.static(dir))
+const dir = path.join(__dirname, 'public/images');
+app.use('/static', express.static(dir))
 
 const dataPath = path.join(__dirname, 'drivers.json');
-const drivers = JSON.parse(fs.readFileSync(dataPath).toString())
+let drivers = JSON.parse(fs.readFileSync(dataPath).toString())
+
+const usedNumbers = new Set()
+drivers = drivers.map(driver => {
+    let randomPlace = Math.floor((Math.random() * drivers.length) + 1)
+    while (usedNumbers.has(randomPlace)) {
+        randomPlace = Math.floor((Math.random() * drivers.length) + 1)
+    }
+    usedNumbers.add(randomPlace)
+    return {
+        ...driver,
+        imgUrl: `/static/${driver.code.toLowerCase()}.png`,
+        place: randomPlace
+    }
+})
+
 
 app.get('/api/drivers', (req, res) => {
     res.send(drivers)

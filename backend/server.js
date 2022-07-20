7b28c75bcc437667dec493fe936b3ca382ce738d
Johnny <johnny@edgeservpos.com>
2022-07-20 21:00:33 +0200

    Added images and driver data to project; Implemented static serving of images and driver data

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,19 @@
-console.log('Server...')
+const express = require('express')
+const dotenv = require('dotenv').config()
+const path = require('path');
+const fs = require('fs');
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+const dir = path.join(__dirname, 'public');
+app.use(express.static(dir))
+
+const dataPath = path.join(__dirname, 'drivers.json');
+const drivers = JSON.parse(fs.readFileSync(dataPath).toString())
+
+app.get('/api/drivers', (req, res) => {
+    res.send(drivers)
+})
+
+app.listen(PORT, () => console.log('Server started.'))
